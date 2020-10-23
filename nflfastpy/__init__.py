a84cca062f90f4a3b56b4e1011e1495859d2a03f
Ben Dominguez <ben@fantasyfootballdatapros.com>
2020-10-23 12:49:33 -0400

    updated import error caused by package name change

--- nflfastpy/__init__.py
+++ nflfastpy/__init__.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from nflfast_py.config import BASE_URL
+from nflfastpy.config import BASE_URL
 
 def load_pbp_data(year=2020):
     df = pd.read_csv(BASE_URL.format(year=year), compression='gzip', low_memory=False)
