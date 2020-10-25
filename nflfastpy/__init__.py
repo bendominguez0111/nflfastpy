8491fa8b00d4dd55dbef47a8ff77cd56273c97ae
Ben Dominguez <ben@fantasyfootballdatapros.com>
2020-10-25 10:48:14 -0400

    added gsis_id converter, removed helpers for now, added roster data and team logo data

--- nflfastpy/__init__.py
+++ nflfastpy/__init__.py
@@ -1,7 +1,15 @@
 import pandas as pd
-from nflfastpy.config import BASE_URL
+from nflfastpy.config import BASE_URL, ROSTER_URL, TEAM_LOGO_URL
+from nflfastpy.utils import convert_to_gsis_id
 
 def load_pbp_data(year=2020):
     df = pd.read_csv(BASE_URL.format(year=year), compression='gzip', low_memory=False)
     return df
 
+def load_roster_data():
+    df = pd.read_csv(ROSTER_URL, compression='gzip', low_memory=False)
+    return df
+
+def load_team_logo_data():
+    df = pd.read_csv(TEAM_LOGO_URL)
+    return df
---
+++ nflfastpy/utils.py
@@ -0,0 +1,9 @@
+import codecs
+import numpy as np
+
+def convert_to_gsis_id(new_id):
+
+    if type(new_id) == float:
+        return new_id
+        
+    return codecs.decode(new_id[4:-8].replace('-',''),"hex").decode('utf-8')
